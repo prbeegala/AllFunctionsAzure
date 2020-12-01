3f917359fa9405d9aece68e913dcf256f157bd34
Prakash Beegala <prakash.beegala@microsoft.com>
2020-12-01 14:56:07 +0000

    New Repo

--- API Connectors/Functions/SaveAttachmentToOneDrive/SaveAttachmentToOneDrive.cs
+++ API Connectors/Functions/SaveAttachmentToOneDrive/SaveAttachmentToOneDrive.cs
@@ -6,29 +6,43 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using Azure.Connectors.OneDrive;
+using Azure.Connectors.Outlook;
 
-namespace prbeegala.functions
+namespace prbeegala.apiconnection
 {
-    public static class SaveAttachmentToOneDrive
+    public static class OneDriveFiles
     {
-        [FunctionName("SaveAttachmentToOneDrive")]
+        [FunctionName("OneDriveFiles")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
+            var outlookConnectionString = System.Environment.GetEnvironmentVariable("OUTLOOK_CONNECTION", EnvironmentVariableTarget.Process);
+            var oneDriveConnectionString = System.Environment.GetEnvironmentVariable("ONEDRIVE_CONNECTION", EnvironmentVariableTarget.Process);
+
+            var outlookConnector = OutlookConnector.Create(outlookConnectionString);
+            var oneDriveConnector = OneDriveConnector.Create(oneDriveConnectionString);
+//added comments
+            var emails = await outlookConnector.Mail.GetEmailsV2Async(
+                fetchOnlyUnread: true,
+                fetchOnlyWithAttachment: true,
+                includeAttachments: true
+            );
+
+            foreach(var email in emails.Value)
+            {
+                foreach(var attachemnt in email.Attachments)
+                {
+                    var bytes = attachemnt.ContentBytes;
+                    Stream stream = new MemoryStream(bytes);
+
+                    await oneDriveConnector.OneDriveFileData.CreateFileAsync("emailAttachments/", attachemnt.Name, stream);
+                }
+            }
+
             log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
+            string responseMessage = "This HTTP triggered function executed successfully.";
             return new OkObjectResult(responseMessage);
         }
     }
